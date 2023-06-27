ashokaditya/kibana

commit bf3c1b18746dfbb2e924b3de6ebd938f1e2b0a41

refactor and test rule

x-pack/plugins/security_solution/common/endpoint/data_generators/automated_action_generator.ts
@@ -1,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { DeepPartial } from 'utility-types';
+import { merge } from 'lodash';
+import type { EndpointAction } from '../types';
+import { FleetActionGenerator } from './fleet_action_generator';
+
+export class AutomatedActionGenerator extends FleetActionGenerator {
+  /** Generate a random endpoint Action (isolate or unisolate) */
+  generate(overrides: DeepPartial<EndpointAction> = {}): EndpointAction {
+    const timeStamp = overrides['@timestamp'] ? new Date(overrides['@timestamp']) : new Date();
+    return merge(
+      {
+        action_id: this.seededUUIDv4(),
+        '@timestamp': timeStamp.toISOString(),
+        expiration: this.randomFutureDate(timeStamp),
+        type: 'INPUT_ACTION',
+        input_type: 'endpoint',
+        agents: [this.seededUUIDv4()],
+        user_id: 'unknown',
+        rule: {
+          id: 'testid',
+          name: 'testname',
+        },
+        data: {
+          command: 'isolate',
+          comment: this.randomString(15),
+          parameter: undefined,
+          output: undefined,
+        },
+      },
+      overrides as EndpointAction
+    );
+  }
+}

x-pack/plugins/security_solution/common/endpoint/data_loaders/index_endpoint_fleet_actions.ts
@@ -8,7 +8,6 @@
 import type { Client } from '@elastic/elasticsearch';
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { AGENT_ACTIONS_INDEX, AGENT_ACTIONS_RESULTS_INDEX } from '@kbn/fleet-plugin/common';
-import type { ResponseActionsApiCommandNames } from '../service/response_actions/constants';
 import type {
   EndpointAction,
   EndpointActionResponse,
@@ -36,7 +35,6 @@ export interface IndexedEndpointAndFleetActionsForHostResponse {
 export interface IndexEndpointAndFleetActionsForHostOptions {
   numResponseActions?: number;
   alertIds?: string[];
-  overrides?: { data: { command: ResponseActionsApiCommandNames } };
 }
 /**
  * Indexes a random number of Endpoint (via Fleet) Actions for a given host
@@ -72,9 +70,10 @@ export const indexEndpointAndFleetActionsForHost = async (
 
   for (let i = 0; i < total; i++) {
     // create an action
-    const action = fleetActionGenerator.generate({
-      data: { ...options.overrides?.data, comment: 'data generator: this host is bad' },
-    });
+    const action: EndpointAction & { rule?: { name: string; id: string } } =
+      fleetActionGenerator.generate({
+        data: { comment: 'data generator: this host is bad' },
+      });
 
     action.agents = [agentId];
     const indexFleetActions = esClient
@@ -110,6 +109,7 @@ export const indexEndpointAndFleetActionsForHost = async (
       user: {
         id: action.user_id,
       },
+      ...(action.rule ? { rule: action.rule } : {}),
     };
 
     await Promise.all([

x-pack/plugins/security_solution/common/endpoint/data_loaders/index_endpoint_hosts.ts
@@ -12,6 +12,7 @@ import { v4 as uuidv4 } from 'uuid';
 import type { KbnClient } from '@kbn/test';
 import type { DeleteByQueryResponse } from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import type { Agent, CreatePackagePolicyResponse, GetInfoResponse } from '@kbn/fleet-plugin/common';
+import { AutomatedActionGenerator } from '../data_generators/automated_action_generator';
 import { EndpointDocGenerator } from '../generate_data';
 import type { HostMetadata, HostPolicyResponse } from '../types';
 import type {
@@ -200,18 +201,14 @@ export async function indexEndpointHostDocs({
 
       if (withResponseActions) {
         // Create some fleet endpoint actions and .logs-endpoint actions for this Host
+        const responseActionsFleetActionGenerator = new AutomatedActionGenerator();
         const actionsResponse = await indexEndpointAndFleetActionsForHost(
           client,
           hostMetadata,
-          undefined,
+          alertIds ? responseActionsFleetActionGenerator : undefined,
           {
-            alertIds,
+            ...(alertIds ? { alertIds } : {}),
             numResponseActions,
-            overrides: {
-              data: {
-                command: 'isolate',
-              },
-            },
           }
         );
         mergeAndAppendArrays(response, actionsResponse);

x-pack/plugins/security_solution/common/endpoint/data_generators/fleet_action_generator.ts
@@ -33,9 +33,7 @@ export class FleetActionGenerator extends BaseDataGenerator {
         agents: [this.seededUUIDv4()],
         user_id: 'elastic',
         data: {
-          command: overrides.data?.command
-            ? overrides.data?.command
-            : this.randomResponseActionCommand(),
+          command: this.randomResponseActionCommand(),
           comment: this.randomString(15),
           parameter: undefined,
           output: undefined,

x-pack/plugins/security_solution/public/management/cypress/e2e/mocked_data/automated_response_actions/history_log.cy.ts
@@ -32,7 +32,6 @@ describe('History Log', () => {
       alertData = indexedAlert;
       const alertId = alertData.alerts[0]._id;
       return indexEndpointHosts({
-        withResponseActions: true,
         numResponseActions: 1,
         alertIds: [alertId],
       }).then((indexEndpoints) => {
@@ -70,6 +69,7 @@ describe('History Log', () => {
     cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
     cy.getByTestSubj('response-actions-list').within(() => {
       cy.get('tbody .euiTableRow').should('have.lengthOf', 1);
+      cy.get('tbody .euiTableRow').eq(0).contains('Triggered by rule');
     });
     cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
     cy.getByTestSubj('response-actions-list').within(() => {
@@ -82,6 +82,9 @@ describe('History Log', () => {
     cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
     cy.getByTestSubj('response-actions-list').within(() => {
       cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength);
+      cy.get('tbody .euiTableRow').eq(0).contains('Triggered by rule').click();
     });
+    // check if we were moved to Rules app after clicking Triggered by rule
+    cy.getByTestSubj('breadcrumb last').contains('Rules');
   });
 });

x-pack/plugins/security_solution/public/management/cypress/e2e/mocked_data/automated_response_actions/no_license.cy.ts
@@ -15,7 +15,7 @@ import { indexEndpointHosts } from '../../../tasks/index_endpoint_hosts';
 import { indexEndpointRuleAlerts } from '../../../tasks/index_endpoint_rule_alerts';
 
 describe('No License', { env: { ftrConfig: { license: 'basic' } } }, () => {
-  describe('User without license should not be able to use response actions even though RBAC is correct', () => {
+  describe('User cannot use endpoint action in form', () => {
     const [ruleName, ruleDescription] = generateRandomStringName(2);
 
     before(() => {

commit ee06d042e7ea8b2bce9d576c536c0b604c7e9517

use endpoint action generator to use rules info
In order to allow adding rule details (id and name) for automated actions, when loading fake response actions and responses this change will start with an endpoint action and uses values from that to create a fleet action doc. Since fleet actions and endpoint actions are identical (expect for the structure), this should not affect other tests or earlier workflows.

This change is to avoid ambiguity of types caused with bf3c1b18746dfbb2e924b3de6ebd938f1e2b0a41.

More details https://github.com/elastic/kibana/pull/160044/commits/bf3c1b18746dfbb2e924b3de6ebd938f1e2b0a41#r1239715594

x-pack/plugins/security_solution/common/endpoint/data_generators/automated_action_generator.ts
@@ -1,40 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import type { DeepPartial } from 'utility-types';
-import { merge } from 'lodash';
-import type { EndpointAction } from '../types';
-import { FleetActionGenerator } from './fleet_action_generator';
-
-export class AutomatedActionGenerator extends FleetActionGenerator {
-  /** Generate a random endpoint Action (isolate or unisolate) */
-  generate(overrides: DeepPartial<EndpointAction> = {}): EndpointAction {
-    const timeStamp = overrides['@timestamp'] ? new Date(overrides['@timestamp']) : new Date();
-    return merge(
-      {
-        action_id: this.seededUUIDv4(),
-        '@timestamp': timeStamp.toISOString(),
-        expiration: this.randomFutureDate(timeStamp),
-        type: 'INPUT_ACTION',
-        input_type: 'endpoint',
-        agents: [this.seededUUIDv4()],
-        user_id: 'unknown',
-        rule: {
-          id: 'testid',
-          name: 'testname',
-        },
-        data: {
-          command: 'isolate',
-          comment: this.randomString(15),
-          parameters: undefined,
-          output: undefined,
-        },
-      },
-      overrides as EndpointAction
-    );
-  }
-}

x-pack/plugins/security_solution/common/endpoint/data_loaders/index_endpoint_fleet_actions.ts
@@ -15,11 +15,13 @@ import type {
   LogsEndpointAction,
   LogsEndpointActionResponse,
 } from '../types';
-import { ENDPOINT_ACTIONS_INDEX, ENDPOINT_ACTION_RESPONSES_INDEX } from '../constants';
+import { ENDPOINT_ACTION_RESPONSES_INDEX, ENDPOINT_ACTIONS_INDEX } from '../constants';
 import { FleetActionGenerator } from '../data_generators/fleet_action_generator';
 import { wrapErrorAndRejectPromise } from './utils';
+import { EndpointActionGenerator } from '../data_generators/endpoint_action_generator';
 
-const defaultFleetActionGenerator = new FleetActionGenerator();
+const fleetActionGenerator = new FleetActionGenerator();
+const endpointActionGenerator = new EndpointActionGenerator();
 
 export interface IndexedEndpointAndFleetActionsForHostResponse {
   actions: EndpointAction[];
@@ -36,18 +38,18 @@ export interface IndexEndpointAndFleetActionsForHostOptions {
   numResponseActions?: number;
   alertIds?: string[];
 }
+
 /**
  * Indexes a random number of Endpoint (via Fleet) Actions for a given host
- * (NOTE: ensure that fleet is setup first before calling this loading function)
+ * (NOTE: ensure that fleet is set up first before calling this loading function)
  *
  * @param esClient
  * @param endpointHost
- * @param [fleetActionGenerator]
+ * @param options
  */
 export const indexEndpointAndFleetActionsForHost = async (
   esClient: Client,
   endpointHost: HostMetadata,
-  fleetActionGenerator: FleetActionGenerator = defaultFleetActionGenerator,
   options: IndexEndpointAndFleetActionsForHostOptions = {}
 ): Promise<IndexedEndpointAndFleetActionsForHostResponse> => {
   const ES_INDEX_OPTIONS = { headers: { 'X-elastic-product-origin': 'fleet' } };
@@ -66,55 +68,59 @@ export const indexEndpointAndFleetActionsForHost = async (
   };
 
   for (let i = 0; i < total; i++) {
-    // create an action
-    const action: EndpointAction & { rule?: { name: string; id: string } } =
-      fleetActionGenerator.generate({
+    // start with endpoint action
+    const logsEndpointAction: LogsEndpointAction = endpointActionGenerator.generate({
+      EndpointActions: {
         data: { comment: 'data generator: this host is bad' },
-      });
+      },
+    });
 
-    action.agents = [agentId];
+    const fleetAction: EndpointAction = {
+      ...logsEndpointAction.EndpointActions,
+      '@timestamp': logsEndpointAction['@timestamp'],
+      agents:
+        typeof logsEndpointAction.agent.id === 'string'
+          ? [logsEndpointAction.agent.id]
+          : logsEndpointAction.agent.id,
+      user_id: logsEndpointAction.user.id,
+    };
+
+    // index fleet action
     const indexFleetActions = esClient
-      .index(
+      .index<EndpointAction>(
         {
           index: AGENT_ACTIONS_INDEX,
-          body: action,
+          body: fleetAction,
           refresh: 'wait_for',
         },
         ES_INDEX_OPTIONS
       )
       .catch(wrapErrorAndRejectPromise);
 
-    const endpointActionsBody: LogsEndpointAction & {
-      EndpointActions: LogsEndpointAction['EndpointActions'] & {
-        '@timestamp': undefined;
-        user_id: undefined;
-      };
-    } = {
+    const logsEndpointActionsBody: LogsEndpointAction = {
+      ...logsEndpointAction,
       EndpointActions: {
-        ...action,
-        '@timestamp': undefined,
-        user_id: undefined,
+        ...logsEndpointAction.EndpointActions,
         data: {
-          ...action.data,
+          ...logsEndpointAction.EndpointActions.data,
           alert_id: options.alertIds,
         },
       },
-      agent: {
-        id: [agentId],
-      },
-      '@timestamp': action['@timestamp'],
-      user: {
-        id: action.user_id,
-      },
-      rule: action.rule,
+      // to test automated actions in cypress
+      rule: options.alertIds
+        ? {
+            id: 'generated_rule_id',
+            name: 'generated_rule_name',
+          }
+        : logsEndpointAction.rule,
     };
 
     await Promise.all([
       indexFleetActions,
       esClient
-        .index({
+        .index<LogsEndpointAction>({
           index: ENDPOINT_ACTIONS_INDEX,
-          body: endpointActionsBody,
+          body: logsEndpointActionsBody,
           refresh: 'wait_for',
         })
         .catch(wrapErrorAndRejectPromise),
@@ -122,8 +128,8 @@ export const indexEndpointAndFleetActionsForHost = async (
 
     const randomFloat = fleetActionGenerator.randomFloat();
     // Create an action response for the above
-    const actionResponse = fleetActionGenerator.generateResponse({
-      action_id: action.action_id,
+    const fleetActionResponse: EndpointActionResponse = fleetActionGenerator.generateResponse({
+      action_id: logsEndpointAction.EndpointActions.action_id,
       agent_id: agentId,
       action_response: {
         endpoint: {
@@ -136,10 +142,10 @@ export const indexEndpointAndFleetActionsForHost = async (
     });
 
     const indexFleetResponses = esClient
-      .index(
+      .index<EndpointActionResponse>(
         {
           index: AGENT_ACTIONS_RESULTS_INDEX,
-          body: actionResponse,
+          body: fleetActionResponse,
           refresh: 'wait_for',
         },
         ES_INDEX_OPTIONS
@@ -150,8 +156,8 @@ export const indexEndpointAndFleetActionsForHost = async (
     if (randomFloat < 0.7) {
       const endpointActionResponseBody = {
         EndpointActions: {
-          ...actionResponse,
-          data: actionResponse.action_data,
+          ...fleetActionResponse,
+          data: fleetActionResponse.action_data,
           '@timestamp': undefined,
           action_data: undefined,
           agent_id: undefined,
@@ -164,16 +170,16 @@ export const indexEndpointAndFleetActionsForHost = async (
         error:
           randomFloat < 0.1
             ? {
-                message: actionResponse.error,
+                message: fleetActionResponse.error,
               }
             : undefined,
-        '@timestamp': actionResponse['@timestamp'],
+        '@timestamp': fleetActionResponse['@timestamp'],
       };
 
       await Promise.all([
         indexFleetResponses,
         esClient
-          .index({
+          .index<LogsEndpointActionResponse>({
             index: ENDPOINT_ACTION_RESPONSES_INDEX,
             body: endpointActionResponseBody,
             refresh: 'wait_for',
@@ -185,8 +191,8 @@ export const indexEndpointAndFleetActionsForHost = async (
       await indexFleetResponses;
     }
 
-    response.actions.push(action);
-    response.actionResponses.push(actionResponse);
+    response.actions.push(fleetAction);
+    response.actionResponses.push(fleetActionResponse);
   }
 
   // Add edge case fleet actions (maybe)
@@ -198,54 +204,54 @@ export const indexEndpointAndFleetActionsForHost = async (
     };
     // 70% of the time just add either an Isolate -OR- an UnIsolate action
     if (randomFloat < 0.7) {
-      let action: EndpointAction;
+      let fleetAction: EndpointAction;
 
       if (randomFloat < 0.3) {
         // add a pending isolation
-        action = fleetActionGenerator.generateIsolateAction(actionStartedAt);
+        fleetAction = fleetActionGenerator.generateIsolateAction(actionStartedAt);
       } else {
         // add a pending UN-isolation
-        action = fleetActionGenerator.generateUnIsolateAction(actionStartedAt);
+        fleetAction = fleetActionGenerator.generateUnIsolateAction(actionStartedAt);
       }
 
-      action.agents = [agentId];
+      fleetAction.agents = [agentId];
 
       await esClient
-        .index(
+        .index<EndpointAction>(
           {
             index: AGENT_ACTIONS_INDEX,
-            body: action,
+            body: fleetAction,
             refresh: 'wait_for',
           },
           ES_INDEX_OPTIONS
         )
         .catch(wrapErrorAndRejectPromise);
 
-      response.actions.push(action);
+      response.actions.push(fleetAction);
     } else {
       // Else (30% of the time) add a pending isolate AND pending un-isolate
-      const action1 = fleetActionGenerator.generateIsolateAction(actionStartedAt);
-      const action2 = fleetActionGenerator.generateUnIsolateAction(actionStartedAt);
+      const fleetAction1 = fleetActionGenerator.generateIsolateAction(actionStartedAt);
+      const fleetAction2 = fleetActionGenerator.generateUnIsolateAction(actionStartedAt);
 
-      action1.agents = [agentId];
-      action2.agents = [agentId];
+      fleetAction1.agents = [agentId];
+      fleetAction2.agents = [agentId];
 
       await Promise.all([
         esClient
-          .index(
+          .index<EndpointAction>(
             {
               index: AGENT_ACTIONS_INDEX,
-              body: action1,
+              body: fleetAction1,
               refresh: 'wait_for',
             },
             ES_INDEX_OPTIONS
           )
           .catch(wrapErrorAndRejectPromise),
         esClient
-          .index(
+          .index<EndpointAction>(
             {
               index: AGENT_ACTIONS_INDEX,
-              body: action2,
+              body: fleetAction2,
               refresh: 'wait_for',
             },
             ES_INDEX_OPTIONS
@@ -253,7 +259,7 @@ export const indexEndpointAndFleetActionsForHost = async (
           .catch(wrapErrorAndRejectPromise),
       ]);
 
-      response.actions.push(action1, action2);
+      response.actions.push(fleetAction1, fleetAction2);
     }
   }
 

x-pack/plugins/security_solution/common/endpoint/data_loaders/index_endpoint_hosts.ts
@@ -12,7 +12,6 @@ import { v4 as uuidv4 } from 'uuid';
 import type { KbnClient } from '@kbn/test';
 import type { DeleteByQueryResponse } from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import type { Agent, CreatePackagePolicyResponse, GetInfoResponse } from '@kbn/fleet-plugin/common';
-import { AutomatedActionGenerator } from '../data_generators/automated_action_generator';
 import { EndpointDocGenerator } from '../generate_data';
 import type { HostMetadata, HostPolicyResponse } from '../types';
 import type {
@@ -201,16 +200,10 @@ export async function indexEndpointHostDocs({
 
       if (withResponseActions) {
         // Create some fleet endpoint actions and .logs-endpoint actions for this Host
-        const responseActionsFleetActionGenerator = new AutomatedActionGenerator();
-        const actionsResponse = await indexEndpointAndFleetActionsForHost(
-          client,
-          hostMetadata,
-          alertIds ? responseActionsFleetActionGenerator : undefined,
-          {
-            ...(alertIds ? { alertIds } : {}),
-            numResponseActions,
-          }
-        );
+        const actionsResponse = await indexEndpointAndFleetActionsForHost(client, hostMetadata, {
+          alertIds,
+          numResponseActions,
+        });
         mergeAndAppendArrays(response, actionsResponse);
       }
     }